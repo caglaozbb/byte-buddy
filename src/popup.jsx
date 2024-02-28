c40a61a31b23cd06ce6af064a9d29669a0354bf0
caglaozbb <caglaozbb@gmail.com>
2024-02-29 01:18:07 +0300

    feat: pet interactions

--- src/popup.jsx
+++ src/popup.jsx
@@ -8,6 +8,9 @@ import { MdEdit } from "react-icons/md";
 function Popup() {
     const [name, setName] = useState(localStorage.getItem('petName') || 'balim');
     const [editable, setEditable] = useState(false);
+    const [hunger, setHunger] = useState(100);
+    const [game, setGame] = useState(100);
+    const [sleep, setSleep] = useState(100);
 
 
     useEffect(() => {
@@ -16,11 +19,11 @@ function Popup() {
 
     const handleChange = (event) => {
         setName(event.target.value);
-    };
+    }
 
     const handleEditClick = () => {
         setEditable(true);
-    };
+    }
 
     const handleKeyPress = (event) => {
         if (event.key === 'Enter') {
@@ -28,17 +31,50 @@ function Popup() {
             localStorage.setItem('petName', name);
             console.log('Name saved:', name);
         }
-    };
-
-    function handleFoodClick() {
-        console.log('Food button clicked');
-        // food
     }
 
+    function handleFoodClick() {
+        setHunger(Math.min(hunger + 10, 100));
+        console.log('Food button clicked');
+    }
+
+    function handlePlayClick() {
+        setGame(Math.min(game + 10, 100));
+        console.log('Play button clicked');
+    }
+
+    function handleSleepClick() {
+        setSleep(Math.min(sleep + 10, 100));
+        console.log('Sleep button clicked');
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setHunger(prevHunger => Math.max(prevHunger - 5, 0));
+        }, 15 * 60 * 1000); //15 dk (15dk * 60 sn * 1000 msa)
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setGame(prevGame => Math.max(prevGame - 5, 0));
+        }, 5 * 60 * 1000); //5 dk 
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSleep(prevSleep => Math.max(prevSleep - 10, 0));
+        }, 60 * 60 * 1000); //1 saat
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div className="container">
-            <img src={petImage} alt="Petim" style={{ width: '50px', height: 'auto' }} />
+            <img src={petImage} alt="Petim" style={{ width: '50px', height: 'auto', justifyContent: 'center' }} />
             <div style={{ display: 'flex', alignItems: 'center' }}>
                 <div style={{ flex: 1 }}>
                     {editable ? (
@@ -52,23 +88,23 @@ function Popup() {
             <div className="bars-container" style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
                 <div>
                     <div className="bar-container">
-                        <div className="food-bar bar"></div>
+                        <div className="food-bar bar" style={{ height: `${hunger}%`, backgroundColor: hunger > 50 ? '#FDA7DF' : '#f73c7a' }}></div>
                     </div>
                     <button id="foodButton" onClick={handleFoodClick}>Food</button>
                 </div>
 
                 <div>
                     <div className="bar-container">
-                        <div className="play-bar bar"></div>
+                        <div className="play-bar bar" style={{ height: `${game}%`, backgroundColor: game > 50 ? '#48dbfb' : '#4848fb' }}></div>
                     </div>
-                    <button id="playButton">Play</button>
+                    <button id="playButton" onClick={handlePlayClick}>Play</button>
                 </div>
 
                 <div>
                     <div className="bar-container">
-                        <div className="sleep-bar bar"></div>
+                        <div className="sleep-bar bar" style={{ height: `${sleep}%`, backgroundColor: sleep > 50 ? '#1dd1a1' : '#1bbb2b' }}></div>
                     </div>
-                    <button id="sleepButton">Sleep</button>
+                    <button id="sleepButton" onClick={handleSleepClick}>Sleep</button>
                 </div>
             </div>
         </div>
