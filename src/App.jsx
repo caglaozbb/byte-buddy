8bcf791563ee1ec7e6ed989cf5a396332651d4c0
caglaozbb <caglaozbb@gmail.com>
2024-03-30 15:29:03 +0300

    refactor: shop fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css'
 import Popup from './popup';
-import Pet from './pet'
 
 function App() {
 
   return (
     <>
-      {/* <div>
-        <Pet />
-      </div> */}
+
       <div>
         <Popup />
       </div>
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const HUNGER_COOLDOWN = 3 * 60 * 1000
+export const HUNGER_COOLDOWN = 5 * 60 * 1000
 export const GAME_COOLDOWN = 2 * 60 * 1000
-export const SLEEP_COOLDOWN = 5 * 60 * 1000
+export const SLEEP_COOLDOWN = 7 * 60 * 1000
 export const PET_VISUAL = 2 * 60 * 1000
 export const DEFAULT_STAT = 100
 export const DEFAULT_INCREMENT_VALUE = 10
