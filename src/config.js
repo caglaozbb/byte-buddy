3fd264edf1f98590cf0f17538f7623737e6760e7
caglaozbb <caglaozbb@gmail.com>
2024-04-14 17:43:11 +0300

    feat: add dark mode support

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-export const HUNGER_COOLDOWN = 60 * 1000
-export const GAME_COOLDOWN = 30 * 1000
-export const SLEEP_COOLDOWN = 3 * 60 * 1000
+export const HUNGER_COOLDOWN = 20 * 1000
+export const GAME_COOLDOWN = 10 * 1000
+export const SLEEP_COOLDOWN = 40 * 1000
 export const PET_VISUAL = 2 * 60 * 1000
 export const DEFAULT_STAT = 100
-export const DEFAULT_INCREMENT_VALUE = 2
+export const DEFAULT_INCREMENT_VALUE = 5
 export const PET_STATUS = {
     ALIVE: "ALIVE",
     DEAD: "DEAD"
